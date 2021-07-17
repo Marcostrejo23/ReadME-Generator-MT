35612bec7d3fbd953c6b755cecd476c5fff6125f
Marcos Trejo <marcostrejo23@gmail.com>
2021-07-16 21:15:11 -0700

    pushing functiong to write file

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
-const inquirer = requirer (`inquirer`);
-const fs = requirer (`fs`);
+const inquirer = require (`inquirer`);
+const fs = require (`fs`);
+const generate  = require ("./util/generateMarkdown");
+// let generateMarkdown = require ();
 
 // TODO: Create an array of questions for user input
 const questions = inquirer.prompt(
@@ -9,6 +11,16 @@ const questions = inquirer.prompt(
             type: `input`,
             message: `what's the name of your project?`,
             name: `title`,
+        },
+        {
+            type: `input`,
+            message: `what is your GitHub username?`,
+            name: `username`,
+        },
+        {
+            type: `input`,
+            message: `what is your email?`,
+            name: `email`,
         },
         {
             type: `input`,
@@ -36,14 +48,48 @@ const questions = inquirer.prompt(
             message: `what are the test instructions?`,
             name: `test`,
         },
+        {
+            type: `checkbox`,
+            message: `what type of license should your project have?`,
+            name: `license`,
+            choices: [
+                "MIT",
+                "APACHE 2.0",
+                "GPL 3.0",
+                "BSD 3",
+                "none"
+            ]
+        },
         
     ]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, data, (err) => {
+        if (err) {
+          console.error("File was not created");
+        } else {
+          console.log("README created succesfully");
+        }
+      });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
+function init() {
 
+    promptUser()
+  
+      .then(function (data) {
+        api.apiCall(data.username)
+          .then(function (avatar) {
+            const url = (avatar.data.avatar_url)
+            console.log(data)
+            return writeFileAsync("TestREADME.md", generateMarkdown(data, url));
+          })
+  
+      })
+  
+  }
 // Function call to initialize app
 init();
--- index.js
+++ index.js
@@ -1,39 +1,54 @@
-const inquirer = requirer (`inquirer`);
-const fs = requirer (`fs`);
+const inquirer = require (`inquirer`);
+const fs = require (`fs`);
+const axios = require ("axios");
 
+const api = {
+    apiCall(username) {
+        return axios.get('https://api.github.com/users/' + username)
+        .catch(error => {
+            console.error(error);
+        });
+    }
+}
 
-inquirer.prompt(
-    [
-        {
-            type: `input`,
-            message: `what's the name of your project?`,
-            name: `title`,
-        },
-        {
-            type: `input`,
-            message: `please describe your product`,
-            name: `description`,
-        },
-        {
-            type: `input`,
-            message: `what are the instalation instructions?`,
-            name: `install`,
-        },
-        
-        {
-            type: `input`,
-            message: `what is the usage information?`,
-            name: `usage`,
-        },
-        {
-            type: `input`,
-            message: `who contributed to the application?`,
-            name: `contribute`,
-        },
-        {
-            type: `input`,
-            message: `what are the test instructions?`,
-            name: `test`,
-        },
-        
-    ]);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+module.exports = api;
+
+function userPrompt(){
+    inquirer.prompt(
+        [
+            {
+                type: `input`,
+                message: `what's the name of your project?`,
+                name: `title`,
+            },
+            {
+                type: `input`,
+                message: `please describe your product`,
+                name: `description`,
+            },
+            {
+                type: `input`,
+                message: `what are the instalation instructions?`,
+                name: `install`,
+            },
+            
+            {
+                type: `input`,
+                message: `what is the usage information?`,
+                name: `usage`,
+            },
+            {
+                type: `input`,
+                message: `who contributed to the application?`,
+                name: `contribute`,
+            },
+            {
+                type: `input`,
+                message: `what are the test instructions?`,
+                name: `test`,
+            },
+            
+        ]);
+    }
