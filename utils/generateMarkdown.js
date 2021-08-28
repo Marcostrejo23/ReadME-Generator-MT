7552cd98effe9ad97be4f769624e75ca924ec4af
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-28 14:53:48 -0700

    generate markdown complete

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,12 +5,24 @@ let LicenseLink
 let licenseSection 
 
 function renderLicenseBadge(license) {
-  if(data.license !== "There is no license")
+  switch(license){
+    case "Apache-2.0":
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    case "GPL 3.0":
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    case "MIT":
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      default:
+        return ``;
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `# License
+  ${renderLicenseBadge(license)}`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -18,9 +30,30 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+ const(title,username,email,description,install,usage,contribute,test,license,)= data;
+ return `# ${title}
+ ## Description
+ $(description)
+##Table of contents
+-[Installation](#Installation)
+-[Usage](#Usage)
+-[Testing](#Testing)
+-[Contributing](#Contributing)
+-[Questions](#Questions)
+-[License](#License)
+##Installation
+${install}
+##Usage
+${usage}
+##Testing
+${test}
+##contributing
+${contribute}
+##Questions
+[Github](https://github.com/${username})
+[Email](${email})
+${renderLicenseSection(license)}`;
 
-`;
 }
 
 module.exports = generateMarkdown;
